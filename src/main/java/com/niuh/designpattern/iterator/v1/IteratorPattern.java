06c99fa22f0b7686498d08fa2d2fee48b724d1b7
hejianhui <org_hejianhui@163.com>
2020-11-06 09:10:00 +0800

    add 迭代器模式

---
+++ src/main/java/com/niuh/designpattern/iterator/v1/IteratorPattern.java
@@ -0,0 +1,94 @@
+package com.niuh.designpattern.iterator.v1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <p>
+ * 迭代器模式
+ * </p>
+ */
+public class IteratorPattern {
+    public static void main(String[] args) {
+        Aggregate ag = new ConcreteAggregate();
+        ag.add("北京");
+        ag.add("上海");
+        ag.add("深圳");
+        System.out.print("聚合的内容有：");
+        Iterator it = ag.getIterator();
+        while (it.hasNext()) {
+            Object ob = it.next();
+            System.out.print(ob.toString() + "\t");
+        }
+        Object ob = it.first();
+        System.out.println("\nFirst：" + ob.toString());
+    }
+}
+
+//抽象迭代器
+interface Iterator {
+    Object first();
+
+    Object next();
+
+    boolean hasNext();
+}
+
+//具体迭代器
+class ConcreteIterator implements Iterator {
+    private List<Object> list = null;
+    private int index = -1;
+
+    public ConcreteIterator(List<Object> list) {
+        this.list = list;
+    }
+
+    public boolean hasNext() {
+        if (index < list.size() - 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public Object first() {
+        index = 0;
+        Object obj = list.get(index);
+        ;
+        return obj;
+    }
+
+    public Object next() {
+        Object obj = null;
+        if (this.hasNext()) {
+            obj = list.get(++index);
+        }
+        return obj;
+    }
+}
+
+//抽象聚合
+interface Aggregate {
+    public void add(Object obj);
+
+    public void remove(Object obj);
+
+    public Iterator getIterator();
+}
+
+//具体聚合
+class ConcreteAggregate implements Aggregate {
+    private List<Object> list = new ArrayList<Object>();
+
+    public void add(Object obj) {
+        list.add(obj);
+    }
+
+    public void remove(Object obj) {
+        list.remove(obj);
+    }
+
+    public Iterator getIterator() {
+        return (new ConcreteIterator(list));
+    }
+}
